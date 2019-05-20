7c57c44b0f3fd5ad3016382ebc69a152448260e3
Volpicella Alessandro <>
2019-05-20 10:40:41 +0200

    #32 Changed parameter in lgbm model script

--- src/models/lgbm_multiclass_baseline/lgbm_mc_bl.py
+++ src/models/lgbm_multiclass_baseline/lgbm_mc_bl.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def lgbm_train(x_train, x_test, num_leaves=61, max_depth=-1, n_estimators=100, subsample=0.8, min_child_samples=50,
+def lgbm_train(x_train, y_train, num_leaves=61, max_depth=-1, n_estimators=100, subsample=0.8, min_child_samples=50,
                learning_rate=0.05, random_state=2019):
     lgb_model = lgb.LGBMClassifier(boosting_type="gbdt", num_leaves=61, reg_alpha=0, reg_lambda=0.01,
                                    max_depth=-1, n_estimators=100, objective='multiclass', subsample=0.8, colsample_bytree=0.8,
