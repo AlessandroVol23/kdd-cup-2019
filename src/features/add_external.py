3cfbdc9aedfd6fe6e006e456393c971ca2189118
ane <ane.berasategi@gmail.com>
2019-06-07 20:01:43 +0200

    #91 adapted external feat pipeline to new train_test split

--- src/features/add_external.py
+++ src/features/add_external.py
@@ -24,8 +24,12 @@ def add_features(df):
 def main():
     
     print("Reading dataframes")
-    df_train = pd.read_csv("data/raw/data_set_phase1/train_queries.csv")
-    df_test = pd.read_csv("data/raw/data_set_phase1/test_queries.csv")
+    df = pd.read_csv("data/raw/data_set_phase1/train_queries.csv")
+    # df_test = pd.read_csv("data/raw/data_set_phase1/test_queries.csv")
+
+    mask = df['req_time'] >= '2018-11-14'
+    df_train = df[~mask]
+    df_test = df[mask]
     
     print("Adding features in df_train")
     df_train = add_features(df_train)
@@ -33,13 +37,11 @@ def main():
     print("Added all features in df_train")
     df_train.to_pickle("data/external_features/train_external.pickle")
 
-    '''
     print("Adding features in df_test")
     df_test = add_features(df_test)
 
     print("Added all features in df_test")
     df_test.to_pickle("data/external_features/test_external.pickle")
-    '''
     
     return
 if __name__ == "__main__":
