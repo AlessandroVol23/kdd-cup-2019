1cd4633192117ce5eadb72ff55621e2f9e046894
Lynn Nguyen <th.nguyen@campus.lmu.de>
2019-06-18 19:38:05 +0200

    74 downsampling including click_mode 0
    74-downsampling update
    
    deleted dvc

--- src/data/make_raw_preprocessing.py
+++ src/data/make_raw_preprocessing.py
@@ -7,14 +7,17 @@ import logging
 
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 from pandas.io.json import json_normalize
 
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-def read_in_data(absolute_raw_data_path):
+def read_in_train_data(absolute_raw_data_path):
     """
-        This function reads in all the data sets.
-        returns: df_train_queries, df_train_plans, df_train_clicks, df_test_queries, df_test_queries, df_test_plans, df_profiles
+        This function reads in all the train data sets.
+        returns: df_train_queries, df_train_plans, df_train_clicks, id_profiles
     """
 
     data_set_path = os.path.join(absolute_raw_data_path, 'raw/data_set_phase1')
@@ -24,9 +27,17 @@ def read_in_data(absolute_raw_data_path):
     df_train_plans = pd.read_csv(os.path.join(data_set_path, "train_plans.csv"))
     df_train_queries = pd.read_csv(os.path.join(data_set_path, "train_queries.csv"))
 
+    return (df_profiles, df_train_queries, df_train_plans, df_train_clicks)
+
+def read_in_test_data(absolute_raw_data_path):
+    """
+    This function reads in all the test data sets.
+    returns: df_test_queries, df_test_plans
+    """
+    data_set_path = os.path.join(absolute_raw_data_path, 'raw/data_set_phase1')
     df_test_queries = pd.read_csv(os.path.join(data_set_path, "test_queries.csv"))
     df_test_plans = pd.read_csv(os.path.join(data_set_path, "test_plans.csv"))
-    return (df_profiles, df_train_queries, df_train_plans, df_train_clicks, df_test_queries, df_test_plans)
+    return (df_test_queries, df_test_plans)
 
 
 def write_data(absolute_raw_data_path, df, train_mode, df_mode, plan_mode='col'):
@@ -111,11 +122,18 @@ def raw_preprocessing(df, plandf, profiledf, clickdf=None, df_mode='col', plan_m
             else:
                 df = df_plans.copy()
             df = pd.merge(df, df_queries, on="sid")
+            #df = pd.merge(df, df_profiles, how='outer')
+            df = df[pd.notnull(df['o_long'])]
 
         else:
             print("Wrong df_mode, try with 'col' or 'row'")
             sys.exit(-1)
         
+        df.pid = df.pid.apply(lambda x: 0 if np.isnan(x) else x)
+        if df_mode == 'col':
+            for i in range(66):
+                df['p'+str(i)] = df['p'+str(i)].apply(lambda x: -1 if np.isnan(x) else x)
+
         return df
 
     '''
@@ -141,6 +159,15 @@ def raw_preprocessing(df, plandf, profiledf, clickdf=None, df_mode='col', plan_m
         df_unstacked.rename({"distance": "distance_plan"}, axis=1, inplace=True)
         return df_unstacked
 
+    def fill_missing_price(df, median=True, mean=False):
+        """
+            This function fills all missing values in price with the median value. 
+        """
+        df.price = pd.to_numeric(df.price)
+        df.loc[df.price.isna(), "price"] = df.price.median()
+        return df
+
+    # DEPRECATED
     '''
     for col mode, unstack plans in columns, necessary for random forest classifier
     '''
@@ -195,21 +222,193 @@ def raw_preprocessing(df, plandf, profiledf, clickdf=None, df_mode='col', plan_m
                 else:
                     df.at[i,'price_' + str(pl['transport_mode'])] = 99999
                 df.at[i,'eta_' + str(pl['transport_mode'])] = pl['eta']
-        print("\n")
         return df
 
+    def gen_plan_feas(data, col_name='plans'):
+        n = data.shape[0]
+        mode_list_feas = np.zeros((n, 12))
+        max_dist, min_dist, mean_dist, std_dist = np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+        max_price, min_price, mean_price, std_price = np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+        max_eta, min_eta, mean_eta, std_eta = np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+        min_dist_mode, max_dist_mode, min_price_mode, max_price_mode, min_eta_mode, max_eta_mode, first_mode = np.zeros(
+        (n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+        mode_texts = []
+        
+        for i, plan in tqdm(enumerate(data[col_name].values)):
+            try:
+                cur_plan_list = json.loads(plan)
+            except:
+                cur_plan_list = []
+            if len(cur_plan_list) == 0:
+                mode_list_feas[i, 0] = 1
+                first_mode[i] = 0
+
+                max_dist[i] = -1
+                min_dist[i] = -1
+                mean_dist[i] = -1
+                std_dist[i] = -1
+
+                max_price[i] = -1
+                min_price[i] = -1
+                mean_price[i] = -1
+                std_price[i] = -1
+
+                max_eta[i] = -1
+                min_eta[i] = -1
+                mean_eta[i] = -1
+                std_eta[i] = -1
+
+                min_dist_mode[i] = -1
+                max_dist_mode[i] = -1
+                min_price_mode[i] = -1
+                max_price_mode[i] = -1
+                min_eta_mode[i] = -1
+                max_eta_mode[i] = -1
+
+                mode_texts.append('word_null')
+            else:
+                distance_list = []
+                price_list = []
+                eta_list = []
+                mode_list = []
+                for tmp_dit in cur_plan_list:
+                    distance_list.append(int(tmp_dit['distance']))
+                    if tmp_dit['price'] == '':
+                        price_list.append(0)
+                    else:
+                        price_list.append(int(tmp_dit['price']))
+                    eta_list.append(int(tmp_dit['eta']))
+                    mode_list.append(int(tmp_dit['transport_mode']))
+                mode_texts.append(' '.join(['word_{}'.format(mode) for mode in mode_list]))
+                distance_list = np.array(distance_list)
+                price_list = np.array(price_list)
+                eta_list = np.array(eta_list)
+                mode_list = np.array(mode_list, dtype='int')
+                mode_list_feas[i, mode_list] = 1
+                distance_sort_idx = np.argsort(distance_list)
+                price_sort_idx = np.argsort(price_list)
+                eta_sort_idx = np.argsort(eta_list)
+
+                max_dist[i] = distance_list[distance_sort_idx[-1]]
+                min_dist[i] = distance_list[distance_sort_idx[0]]
+                mean_dist[i] = np.mean(distance_list)
+                std_dist[i] = np.std(distance_list)
+
+                max_price[i] = price_list[price_sort_idx[-1]]
+                min_price[i] = price_list[price_sort_idx[0]]
+                mean_price[i] = np.mean(price_list)
+                std_price[i] = np.std(price_list)
+
+                max_eta[i] = eta_list[eta_sort_idx[-1]]
+                min_eta[i] = eta_list[eta_sort_idx[0]]
+                mean_eta[i] = np.mean(eta_list)
+                std_eta[i] = np.std(eta_list)
+
+                first_mode[i] = mode_list[0]
+                max_dist_mode[i] = mode_list[distance_sort_idx[-1]]
+                min_dist_mode[i] = mode_list[distance_sort_idx[0]]
+
+                max_price_mode[i] = mode_list[price_sort_idx[-1]]
+                min_price_mode[i] = mode_list[price_sort_idx[0]]
+
+                max_eta_mode[i] = mode_list[eta_sort_idx[-1]]
+                min_eta_mode[i] = mode_list[eta_sort_idx[0]]
+
+        feature_data = pd.DataFrame(mode_list_feas)
+        feature_data.columns = ['mode_{}_available'.format(i) for i in range(12)]
+        feature_data['max_dist'] = max_dist
+        feature_data['min_dist'] = min_dist
+        feature_data['mean_dist'] = mean_dist
+        feature_data['std_dist'] = std_dist
+
+        feature_data['max_price'] = max_price
+        feature_data['min_price'] = min_price
+        feature_data['mean_price'] = mean_price
+        feature_data['std_price'] = std_price
+
+        feature_data['max_eta'] = max_eta
+        feature_data['min_eta'] = min_eta
+        feature_data['mean_eta'] = mean_eta
+        feature_data['std_eta'] = std_eta
+
+        feature_data['max_dist_mode'] = max_dist_mode
+        feature_data['min_dist_mode'] = min_dist_mode
+        feature_data['max_price_mode'] = max_price_mode
+        feature_data['min_price_mode'] = min_price_mode
+        feature_data['max_eta_mode'] = max_eta_mode
+        feature_data['min_eta_mode'] = min_eta_mode
+        feature_data['first_mode'] = first_mode
+        
+        print('mode tfidf...')
+        tfidf_enc = TfidfVectorizer(ngram_range=(1, 2))
+        tfidf_vec = tfidf_enc.fit_transform(mode_texts)
+        svd_enc = TruncatedSVD(n_components=10, n_iter=20, random_state=2019)
+        mode_svd = svd_enc.fit_transform(tfidf_vec)
+        mode_svd = pd.DataFrame(mode_svd)
+        mode_svd.columns = ['TFIDF_clustered_{}'.format(i) for i in range(10)]
+        data = pd.concat([data, feature_data, mode_svd], axis=1)
+            
+        #data = data.drop([col_name], axis=1)
+        return data
+
+    def gen_profile_feas(data, profile_data):
+        
+        # add "-1" as new PID, that has "-1" on all p0,...p65
+        profile_data.loc[len(profile_data)] = list(np.repeat(-1, 67))
+        x = profile_data.drop(['pid'], axis=1).values
+        svd = TruncatedSVD(n_components=20, n_iter=20, random_state=2019)
+        svd_x = svd.fit_transform(x)
+        svd_feas = pd.DataFrame(svd_x)
+        newcols = ['PID_MainComp_{}'.format(i) for i in range(20)]
+        svd_feas.columns = newcols
+        svd_feas['pid'] = profile_data['pid'].values
+        data['pid'] = data['pid'].fillna(-1)
+        data = data.merge(svd_feas, on='pid', how='left')
+        return data
+
+    def gen_time_feas(data):
+        data['req_time'] = pd.to_datetime(data['req_time'])
+        data['weekday'] = data['req_time'].dt.dayofweek
+        data['hour'] = data['req_time'].dt.hour
+        return data
+
+    def split_train_test_by_col(df):
+        mask = df['req_time'] >= '2018-11-14'
+        train = df[~mask]
+        test = df[mask]
+        train = train.drop(['req_time'], axis=1)
+        test = test.drop(['req_time'], axis=1)
+        return (train, test)
+
+    def split_train_test_by_row(df):
+        df.sort_values(by=['req_time'], inplace=True)
+        limit = int(0.8*df.shape[0])
+        traindf = df.head(limit)
+        testdf = df.tail(len(df) - limit)
+        return traindf, testdf
+
     '''
+    **********************
     Preprocessing pipeline
+    **********************
     '''
 
-    print("Preprocessing coordinates")
+    print("1. Preprocessing coordinates and time")
     df = preprocess_coordinates(df)
+    df = gen_time_feas(df)
 
     if df_mode == 'col':
-        print("Preprocessing df in 'col' mode")
+        print("2. Preprocessing df in 'col' mode")
         plandf, clickdf, df = preprocess_datatypes(plandf, clickdf, df)
+        print("3. Generating profile features")
+        df = gen_profile_feas(df, profiledf)
         df = join_data_sets(plandf, clickdf, df, profiledf, df_mode)
-
+        print("4. Generating plan features")
+        df = gen_plan_feas(df, col_name='plans')
+        
         num_modes = 12
         modes = []
         for i in range(num_modes):
@@ -219,28 +418,45 @@ def raw_preprocessing(df, plandf, profiledf, clickdf=None, df_mode='col', plan_m
 
         df = initialize_plan_cols(df, modes)
         if plan_mode == 'first':
-            print("Preprocessing plans in 'first' mode")
+            print("5. Preprocessing plans in 'first' mode")
             df = preprocess_plans_first(df)
         elif plan_mode == 'last':
-            print("Preprocessing plans in 'last' mode")
+            print("5. Preprocessing plans in 'last' mode")
             df = preprocess_plans_last(df)
         else:
             print("ERROR: wrong plan mode. Try with 'first' or 'last'.")
             sys.exit(1)
 
-
     elif df_mode == 'row':
-        print("Preprocessing df in 'row' mode")
+        print("2. Preprocessing df in 'row' mode")
+        df0 = df.merge(plandf, on='sid', how='left')
+        print("3. Generating profile features")
+        df0 = gen_profile_feas(df0, profiledf)
+        print("4. Generating plan features")
+        df_with_plans = gen_plan_feas(df0)
+        print("5. Unstacking plans into rows")
         df_plans_pp = unstack_plans(plandf)
-        df_plans_pp, clickdf, df = preprocess_datatypes(df_plans_pp, clickdf, df)
-        df = join_data_sets(df_plans_pp, clickdf, df, profiledf, df_mode)
+        df_plans_pp, clickdf, df_with_plans = preprocess_datatypes(df_plans_pp, clickdf, df_with_plans)
+        df = join_data_sets(df_plans_pp, clickdf, df_with_plans, profiledf, df_mode)
+        df = fill_missing_price(df)
     else:
         print("Wrong df mode, try with 'row' or 'col'")
         sys.exit(-1)
 
+    if 'click_mode' in df:
+        print("6. Preprocessing click_mode")
+        df.click_mode = df.click_mode.apply(lambda x: 0 if np.isnan(x) else x)
+        df['Response'] = df.click_mode
+
     if 'plans' in df:
         df = df.drop('plans', axis=1)
-    return df
+    
+    print("7. Split train and test")
+    print(str(df.shape[0]), str(df.shape[1]))
+    #traindf, testdf = split_train_test_by_col(df)
+    traindf, testdf = split_train_test_by_row(df)
+
+    return (traindf, testdf)
 
 @click.command()
 @click.argument("absolute_path_data_folder")
@@ -248,18 +464,21 @@ def raw_preprocessing(df, plandf, profiledf, clickdf=None, df_mode='col', plan_m
 @click.argument("plan_mode")
 def main(absolute_path_data_folder, df_mode, plan_mode):
 
-    df_profiles, df_train_queries, df_train_plans, df_train_clicks, df_test_queries, df_test_plans = read_in_data(absolute_path_data_folder)
+    df_profiles, df_train_queries, df_train_plans, df_train_clicks = read_in_train_data(absolute_path_data_folder)
+    # df_test_queries, df_test_plans = read_in_test_data(absolute_raw_data_path)
     
-    print("Creating raw features for df_train")
-    df_train = raw_preprocessing(df_train_queries, df_train_plans, df_profiles, clickdf=df_train_clicks, df_mode=df_mode, plan_mode=plan_mode)
-    print("Writing train to pickle in ../data/processed_raw/")
-    write_data(absolute_path_data_folder, df_train, 'train', df_mode, plan_mode)
-
-    print("Creating raw features for df_test")
-    df_test = raw_preprocessing(df_test_queries, df_test_plans, df_profiles, df_mode=df_mode, plan_mode=plan_mode)
-    write_data(absolute_path_data_folder, df_test, 'test', df_mode, plan_mode)
+    print("traindf: creating raw features for df_train")
+    df_tr_train, df_tr_test = raw_preprocessing(df_train_queries, df_train_plans, df_profiles, clickdf=df_train_clicks, df_mode=df_mode, plan_mode=plan_mode)
+    write_data(absolute_path_data_folder, df_tr_train, 'train', df_mode, plan_mode)
+    write_data(absolute_path_data_folder, df_tr_test, 'test', df_mode, plan_mode)
 
     return
+    '''DEPRECATED
+    print("\n")
+    print("testdf_ creating raw features for df_test")
+    df_test = raw_preprocessing(df_test_queries, df_test_plans, df_profiles, df_mode=df_mode, plan_mode=plan_mode)
+    write_data(absolute_path_data_folder, df_test, 'test', df_mode, plan_mode)
+    '''
 
 if __name__ == "__main__":
     main()
---
+++ src/features/join_features.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pickle
+import os
+import sys
+
+def main():
+    
+    print("Reading external dataframes")
+    ext = pd.read_pickle('data/external_features/external.pickle')
+
+    ext = ext.drop(['pid', 'req_time'], axis=1)
+    
+    inpath = 'data/processed_raw'
+    outpath = 'data/processed_all'
+
+    print("Processing pickles")
+    for pick in os.listdir(inpath):
+        print("Reading: " + pick)
+        df = pd.read_pickle(os.path.join(inpath, pick))
+        print("Before: " + str(df.shape[0]) + ", " + str(df.shape[1]))
+        newdf = pd.merge(df, ext, how='inner')
+        print("After: " + str(newdf.shape[0]) + ", " + str(newdf.shape[1]))
+        outpick = pick.replace('raw', 'all')
+        print("Writing: " + outpick)
+        newdf.to_pickle(os.path.join(outpath, outpick))
+
+
+if __name__ == "__main__":
+    main()
