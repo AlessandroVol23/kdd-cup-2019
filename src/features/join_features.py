74aed8a8b1fb58cd8ce492dc3f2547712f06100d
ane <a.berasategui@campus.lmu.de>
2019-07-02 09:25:59 +0200

    added pickles to dvc

--- src/features/join_features.py
+++ src/features/join_features.py
@@ -6,23 +6,28 @@ import sys
 def main():
     
     print("Reading external dataframes")
-    ext = pd.read_pickle('data/external_features/external.pickle')
+    ext = pd.read_pickle('data/processed/features/external.pickle')
 
     ext = ext.drop(['pid', 'req_time'], axis=1)
     
-    inpath = 'data/processed_raw'
-    outpath = 'data/processed_all'
+    inpath = ['data/processed/ranking', 'data/processed/multiclass']
 
     print("Processing pickles")
-    for pick in os.listdir(inpath):
-        print("Reading: " + pick)
-        df = pd.read_pickle(os.path.join(inpath, pick))
-        print("Before: " + str(df.shape[0]) + ", " + str(df.shape[1]))
-        newdf = pd.merge(df, ext, how='inner')
-        print("After: " + str(newdf.shape[0]) + ", " + str(newdf.shape[1]))
-        outpick = pick.replace('raw', 'all')
-        print("Writing: " + outpick)
-        newdf.to_pickle(os.path.join(outpath, outpick))
+    for inputdir in inpath:
+        visited_files = []
+        for pick in os.listdir(inputdir):
+            if os.path.splitext(pick)[1] != '.pickle' or pick in visited_files:
+                continue
+            print("Reading: " + pick)
+            df = pd.read_pickle(os.path.join(inputdir, pick))
+            print("Before: " + str(df.shape[0]) + ", " + str(df.shape[1]))
+            newdf = pd.merge(df, ext, how='inner')
+            print("After: " + str(newdf.shape[0]) + ", " + str(newdf.shape[1]))
+            outpick = pick.replace('raw', 'all')
+            print("Writing: " + outpick)
+            newdf.to_pickle(os.path.join(inputdir, outpick))
+            visited_files.append(pick)
+            visited_files.append(outpick)
 
 
 if __name__ == "__main__":
