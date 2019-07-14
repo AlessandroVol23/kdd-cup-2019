9911cb570fbd3a60cac146d0c23f5e329ca57861
Frederik Ludwigs <kuchenmann93@gmail.com>
2019-07-14 13:42:13 +0200

    litte correction with SID splitting for CV

---
+++ src/data/split_SIDs_for_CV.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Script to sample SIDs from the [multiclass-]TrainSet into 'k'-folds.
+[These SIDs are used for the Test/Train Splits in the CrossValidation]
+"""
+# Load needed packages
+import pandas as pd
+import random
+import pickle
+import os
+
+# Check Working Directory:
+if os.getcwd()[-12:] != "kdd-cup-2019":
+	raise ValueError("Error with WorkingDirectory \n \
+			[1] either WD isn't set correctly to 'kdd-cup-2019' \n \
+			[2] or WD was renamed")
+
+#%% Define Function:
+def slice_to_evely_sized_chunks(k):
+	"""
+	slice the 'sampled_ids' into k evely sized chunks!
+	"""
+	# Load the multiclass Traindata & extract the "sid"-Column:
+	train_set = pd.read_pickle("data/processed/multiclass/train_all_first.pickle")
+	all_sids  = train_set["sid"]
+	
+	# Set seed for Reproducibility and save sids in cahnged order!
+	random.seed(9001)
+	sampled_ids = random.sample(list(all_sids), k = len(all_sids))
+	
+	# get the length of each fold & check whether it has no floats:
+	sample_sizes = len(sampled_ids) / k
+	
+	if sample_sizes % 1 != 0:
+		raise ValueError("k can not divide the SIDs into evely sized chunks")
+		
+	# Check whether the folder to save the results is existent:
+	if not os.path.isdir("data/processed/split_test_train/" + str(k) + "-fold"):
+		raise ValueError("There is no " + str(k) + "-fold Folder in data/processed/split_test_train/")
+	
+	# Define List to save SIDs for each fold
+	SIDs = []
+	
+	# Slice the 'sampled_ids' into k evely sized chunks:
+	for fold in range(k):
+		SIDs.append(sampled_ids[int(fold * sample_sizes) : int((fold + 1) * sample_sizes)])
+
+	# Save the Results as Single pickle File:
+	with open("data/processed/split_test_train/" + str(k) + "-fold/SIDs.pickle", "wb") as fp:
+		pickle.dump(SIDs, fp)
+
+# check whether the sampled_ids have the same length as TrainSet["sid"]:
+if len(set(sampled_ids)) != len(set(all_sids)):
+	raise ValueError("Error, sampled SIDs have less elements than TrainSet has SIDs")
+
+
+#%% Create SIDs for CV
+amount_folds = 5
+if __name__ == "__main__":
+	slice_to_evely_sized_chunks(amount_folds)
+
