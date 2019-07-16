120eaa658825ef20c1481d59c7d72095c01eb875
anebz <ane.berasategi@gmail.com>
2019-07-16 09:53:01 +0200

    Deleted additional old src files

--- src/mlflow/test_mlflow.py
+++
@@ -1,8 +0,0 @@
-import logmlflow as x
-import pandas as pd
-
-
-summary = pd.read_csv("../data/Summaries/XGBoost/Summary16.csv")
-
-metrics_dict, parameter_dict, rest_param_dict, feature_dict = x.mlflow_dict(summary)
-x.mlflow_log(2, metrics_dict, parameter_dict, rest_param_dict, feature_dict)
